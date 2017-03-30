d699e387617e50c15ade567d7503eb71da751ad2
Julien Bouvet <julien.bouvet@ekino.com>
2017-03-30 10:24:26 +0200

    Refactoring to handle both mutliple and single archives

--- index.js
+++ index.js
@@ -4,73 +4,89 @@ const GUTIL = require('gulp-util')
 const THROUGH = require('through2')
 const YAZL = require('yazl')
 const GET_STREAM = require('get-stream')
-const EVENT_STREAM = require( 'event-stream' )
+const EVENT_STREAM = require('event-stream')
 
-module.exports = ( filename, opts ) => {
-	const PATH_RADIX = /^([a-z0-9\-\_\s]+)\//i
+module.exports = (filename, opts) => {
+	const FOLDER_NAME_PATTERN = '[a-z0-9\-\_\s\$\,•\+]+'
+	, PATH_RADIX = new RegExp(`^(${FOLDER_NAME_PATTERN})\/`, 'i')
+	, PARENT_RADIX = new RegExp(`\/(${FOLDER_NAME_PATTERN})\/$`, 'i')
 
-	let hasIgnoredRootFolder = false
+	let hasRootFolder = false
 	, archivesCount = 0
+	// , rootLevel = -1
 	, archives = {}
 	, masterPath = ''
 	, pathname = ''
+	, sketchRoot = ''
 	, belongsTo = ''
-	, getRadix = function( pathname = '' ){
-		let radix = pathname.match( PATH_RADIX )
-		if( radix === null || radix.length < 2 ) return pathname
+	, getRadix = function(pathname = ''){
+		let radix = pathname.match(PATH_RADIX)
+		if(radix === null || radix.length < 2) return pathname
 		return radix[ 1 ]
 	}
-	, newArchive = function( name = 'untitled' ){
-		if( !archives[ name ] ){
+	, newArchive = function(name = 'untitled'){
+		if(!archives[ name ]){
 			let zip = new YAZL.ZipFile()
 			archives[ name ] = { id: archivesCount++, zip, name }
 		}
 		return archives[ name ] // we return the zip instance
 	}
-	, zipItOut = function( _that, name = '' ){
+	, zipItOut = function(_that, name = ''){
 		let pName
 		, pBuffer
 		, pAll
-		, writeInPipe = ( name, contents ) => new GUTIL.File({
+		, writeInPipe = (name, contents) => new GUTIL.File({
 				cwd: masterPath
 				, base: masterPath
 				, path: `${masterPath}/${name}.zip`
 				, contents
 			}
 		)
-		, writeArchive = function( name, contents ){
-			_that.push( writeInPipe( name, contents ) )
+		, writeArchive = function(name, contents){
+			_that.push(writeInPipe(name, contents))
 			return contents
 		}
 
 		archives[ name ].zip.end()
-		pName = new Promise( ( resolve, reject ) => resolve( name ) )
-		pBuffer = GET_STREAM.buffer( archives[ name ].zip.outputStream )
+		pName = new Promise((resolve, reject) => resolve(name))
+		pBuffer = GET_STREAM.buffer(archives[ name ].zip.outputStream)
 
 		pBuffer
-			.then( writeArchive.bind( null, name ) )
-			.catch( err => { throw new GUTIL.PluginError( 'gulp-zip', err ) } )
+			.then(writeArchive.bind(null, name))
+			.catch(err => { throw new GUTIL.PluginError('gulp-zip', err) })
 
-		pAll = Promise.all( [ pBuffer, pName ] )
+		pAll = Promise.all([ pBuffer, pName ])
 
 		return pAll
 	}
-	, shorterPath = ( pathname, belongsTo ) => pathname.replace( new RegExp( `${belongsTo}\/`, 'i'), '' )
+	, shorterPath = (pathname, belongsTo) => pathname.replace(new RegExp(`${belongsTo}\/`, 'i'), '')
 	, transformFn = (file, enc, cb) => {
-		// Because Windows...
-		pathname = file.relative.replace(/\\/g, '/')
-		belongsTo = getRadix( pathname )
-		pathname = shorterPath( pathname, belongsTo )
+		let currentZip = null
+		, pathname = file.relative.replace(/\\/g, '/') // Because Windows…
+		, finalPathname = ''
+		, splitPath = file.base.split('/')
+		, captureAll = false
+		belongsTo = getRadix(pathname)
+		pathname = shorterPath(pathname, belongsTo)
 
-		if( masterPath.length === 0 ){
+		if(masterPath.length === 0){
 			masterPath = file.base
-			hasIgnoredRootFolder = true
+			if(!opts.rootAt){
+				opts.rootAt = masterPath
+			}
+			opts.rootAt = opts.rootAt.replace('/', '')
+			sketchRoot = splitPath[ splitPath.length - 2 ]
 		}
+		captureAll = (sketchRoot === opts.rootAt	)
+		finalPathname = (captureAll === true ? pathname : file.relative)
 
-		if( file.isNull() && file.stat && file.stat.isDirectory && file.stat.isDirectory() ){
+		if(file.isNull() && file.stat && file.stat.isDirectory && file.stat.isDirectory()){
 			// IS A DIRECTORY
-			if( pathname === belongsTo ){ // TODO: is an archive!
-				newArchive( pathname ) // feeds a new instance of ZipFile and set archives[].zip with this instance
+			if(captureAll === true){
+				newArchive(belongsTo) // feeds a new instance of ZipFile and set archives[].zip with this instance
+				if(hasRootFolder === false){
+					hasRootFolder = true
+				}
 			}
 		} else {
 			const stat = {
@@ -79,25 +95,31 @@ module.exports = ( filename, opts ) => {
 				, mode: file.stat ? file.stat.mode : null
 			}
 
+			if(!currentZip && archivesCount === 0){ // we never found a root folder, so we'll fall back onto parent
+				currentZip = newArchive(sketchRoot)
+			} else {
+				currentZip = archives[ captureAll === true ? belongsTo : sketchRoot ] // depending on the scenario, the referal zip instance is either itself or root
+			}
+
 			if (file.isStream()) {
-				archives[ belongsTo ].zip.addReadStream(file.contents, pathname, stat)
+				currentZip.zip.addReadStream(file.contents, finalPathname, stat)
 			}
 
 			if (file.isBuffer()) {
-				archives[ belongsTo ].zip.addBuffer(file.contents, pathname, stat)
+				currentZip.zip.addBuffer(file.contents, finalPathname, stat)
 			}
 		}
 		cb()
 	}
-	, flushFn = function( cb ){
-		let names = Object.keys( archives )
+	, flushFn = function(cb){
+		let names = Object.keys(archives)
 		, promises = []
-		for( let name of names ){
-			promises.push( zipItOut( this, name ) )
+		for(let name of names){
+			promises.push(zipItOut(this, name))
 		}
 		Promise
-			.all( promises )
-			.then( () => { GUTIL.log(`${promises.length} archives have been zipped out`); cb(); })
+			.all(promises)
+			.then(() => { GUTIL.log(`${promises.length} archives have been zipped out`); cb(); })
 	}
 
 	opts = Object.assign({
@@ -105,9 +127,9 @@ module.exports = ( filename, opts ) => {
 		, inferFilename: false
 	}, opts)
 
-	if (!filename && opts.inferFilename === true) {
+	if (!filename && opts.inferFilename !== true) {
 		throw new GUTIL.PluginError('gulp-zip', '`filename` required')
 	}
 
-	return THROUGH.obj( transformFn, flushFn )
+	return THROUGH.obj(transformFn, flushFn)
 }
